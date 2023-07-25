87dff33ad9786e0b15c5ef412ad14c60d8292150
taewoo74 <twkim@dkargo.io>
2023-07-25 18:13:33 +0900

    main push

---
+++ src/apis/index.ts
@@ -0,0 +1,5 @@
+// const instance = axios.create({
+//     baseURL: ''https://mvp-api.bleepy.net/api/client-admin/,
+//     headers: { 'X-Custom-Header': 'foobar' },
+//     timeout: 1000,
+//   });
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './views/App.tsx'
+import '../src/styles/index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
